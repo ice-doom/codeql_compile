411647d6a7ae8d985cae0196662527b6aa7e60bd
aw220 <47287118+aw220@users.noreply.github.com>
2022-03-01 17:12:25 +0800

    Update codeql_compile.py
    更新功能

--- codeql_compile.py
+++ codeql_compile.py
@@ -6,11 +6,17 @@ import platform
 import shutil
 
 # 指定自定义java-decompiler路径
-self_java_decompiler_path = r'/Users/x/Downloads/codeql_compile/java-decompiler.jar'
+self_java_decompiler_path = r'/java-decompiler.jar'
 # 指定自定义ecj的路径
-self_ecj_path = r"/Users/x/Downloads/codeql_compile/ecj-4.6.1.jar"
+self_ecj_path = r"/ecj-4.6.1.jar"
 # 指定自定义procyon的路径
-self_procyon_path = r"/x/helu/Downloads/codeql_compile/procyon.jar"
+self_procyon_path = r"/procyon.jar"
+# 指定自定义codeql的路径
+codeql_path = r"/codeql"
+
+
+def get_arch():
+    return platform.system().lower()
 
 
 # 用来校验本地的ecj、java_decompiler路径是否正确
@@ -42,10 +48,7 @@ def java_decompiler_run():
 
 # 先尝试编译成class，定位错误文件再使用procyon反编译替换
 def check():
-    if platform.system().lower() == 'windows':
-        _sub = subprocess.getstatusoutput('{}/run.cmd'.format(save_path))
-    else:
-        _sub = subprocess.getstatusoutput('{}/run.sh'.format(save_path))
+    _sub = subprocess.getstatusoutput('{0}/{1}'.format(save_path,run))
     # 正则匹配错误文件路径
     re_matchs = set(re.findall("ERROR in (.*)? \(at line", _sub[1]))
 
@@ -86,7 +89,7 @@ def check():
     shutil.rmtree(save_path + "/procyon_class")
 
 
-# 创建用来编译的脚本，再codeql创建数据库时使用
+# 创建用来编译的脚本，在codeql创建数据库时使用
 def compile_cmd_file_create():
     # 准备待编译的jar包
     with open("{}/file.txt".format(save_path), "w+") as f:
@@ -113,38 +116,105 @@ def compile_cmd_file_create():
             os.system('chmod u+x {}/run.sh'.format(save_path))
 
 
+def create_database(app_path, include=None, source=None):
+    if source is None:
+        source = app_path
+    if include is None:
+        os.system(
+            '{codeql} database create ../{name} -l \"java\" -s \"{source}\" -c \"{run}\"'.format(
+                app_path=app_path, codeql=codeql_path,
+                name="{0}_database".format(
+                    pathlib.Path(
+                        app_path).name),
+                source=source,
+            run=run))
+    else:
+        os.system("{codeql} database init -s {source} -l \"java\" {name}".format(app_path=app_path,
+                                                                                 codeql=codeql_path,
+                                                                                 name="{0}_database".format(
+                                                                                     pathlib.Path(
+                                                                                         app_path).name),
+                                                                                 source=source))
+        os.system("{codeql} database trace-command {name} {run}".format(app_path=app_path,
+                                                                           codeql=codeql_path,
+                                                                           name="{0}_database".format(
+                                                                               pathlib.Path(
+                                                                                   app_path).name),
+                                                                        run=run
+                                                                           ))
+        extend = None
+        if include == "xml":
+            extend = "xml"
+        os.system(
+            "{codeql} database index-files --language {include} --include-extension .{extend} {name}".format(
+                app_path=app_path,
+                codeql=codeql_path,
+                include=include,
+                extend=extend,
+                name="{0}_database".format(
+                    pathlib.Path(
+                        app_path).name),
+            ))
+        os.system(
+            "{codeql} database finalize {name}".format(
+                app_path=app_path,
+                codeql=codeql_path,
+                include=include,
+                extend=extend,
+                name="{0}_database".format(
+                    pathlib.Path(
+                        app_path).name),
+            ))
+
+
 epilog = r'''Example:
-python3 codeql_compile.py -a D:\java\apps\cloud -d "D:\java\apps\cloud\lib"
-python3 codeql_compile.py -a D:\java\apps\cloud -o "D:\java\apps\cloud_save_1641018608 -c"
+python3 codeql_compile.py -l java -a D:\java\apps\cloud -d "D:\java\apps\cloud\lib"  普通创建
+python3 codeql_compile.py -l java -a D:\java\apps\cloud -d "D:\java\apps\cloud\lib -i xml"  包含xml文件
 '''
 parse = argparse.ArgumentParser(epilog=epilog, formatter_class=argparse.RawDescriptionHelpFormatter)
+parse.add_argument('-l', '--language', help='指定语言')
+parse.add_argument('-i', '--include', help='指定要额外包含的文件类型')
 parse.add_argument('-a', '--app', help='输入项目根路径')
 parse.add_argument('-d', '--dep', help='输入依赖包路径')
 parse.add_argument('-c', '--check', help='对java-decompiler反编译内容进行检测', action="store_true")
 parse.add_argument('-o', '--out', help='输入刚刚已经反编译好的存放路径')
 args = parse.parse_args()
+# 指定语言
+language = args.language
 # 指定源项目路径，就是要被反编译的项目路径
 app_path = args.app
 # 指定依赖包路径，在编译代码时需要指定，否则代码中存在相关依赖会导致编译失败
 dependencies_path = args.dep
 save_path = args.out
-
-if app_path is not None and dependencies_path is not None:
-    ecj_path = verify("ecj.jar", self_ecj_path)
-    java_decompiler_path = verify("java-decompiler.jar", self_java_decompiler_path)
-    if ecj_path is False or java_decompiler_path is False:
-        sys.exit("请在当前目录存放ecj.jar、java-decompiler.jar，或者通过self_java_decompiler_path、self_ecj_path指定自定义路径")
-    save_path = pathlib.Path.joinpath(pathlib.Path(app_path).parent,
-                                      "{}_save_{}".format(pathlib.Path(app_path).name, int(time.time())))
-    save_path.mkdir()
-    java_decompiler_run()
-    compile_cmd_file_create()
-
-elif args.check is not None and app_path is not None and save_path is not None:
-    procyon_path = verify("procyon.jar", self_procyon_path)
-    if procyon_path is False:
-        sys.exit("请在当前目录存放procyon.jar，或者通过self_procyon_path指定自定义路径")
-    check()
+# 指定要包含的文件
+include = args.include
+# 脚本类型
+run = None
+if "windows" == get_arch():
+    run = "./run.sh"
 else:
-    parse.print_help()
-    sys.exit()
+    run = "run.cmd"
+
+if __name__ == '__main__':
+    if language == "java":
+        if app_path is not None and dependencies_path is not None:
+            ecj_path = verify("ecj.jar", self_ecj_path)
+            java_decompiler_path = verify("java-decompiler.jar", self_java_decompiler_path)
+            if ecj_path is False or java_decompiler_path is False:
+                sys.exit("请在当前目录存放ecj.jar、java-decompiler.jar，或者通过self_java_decompiler_path、self_ecj_path指定自定义路径")
+            save_path = pathlib.Path.joinpath(pathlib.Path(app_path).parent,
+                                              "{}_save_{}".format(pathlib.Path(app_path).name, int(time.time())))
+            save_path.mkdir()
+            java_decompiler_run()
+            compile_cmd_file_create()
+            create_database(app_path, include, save_path)
+        elif app_path is not None:
+            create_database(app_path, save_path)
+        elif args.check is not None and app_path is not None and save_path is not None:
+            procyon_path = verify("procyon.jar", self_procyon_path)
+            if procyon_path is False:
+                sys.exit("请在当前目录存放procyon.jar，或者通过self_procyon_path指定自定义路径")
+            check()
+        else:
+            parse.print_help()
+            sys.exit()
